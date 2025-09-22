6773f082dcd1ae0688e1e0af963ee434885ece5a
ma-zn <moaz.zain@outlook.com>
2025-09-22 18:01:26 +0200

    add category and admin and user name

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'created_at')
+    list_filter = ('category', 'created_at')
+    search_fields = ('name', 'description')
+
